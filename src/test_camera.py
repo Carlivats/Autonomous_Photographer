061c849be579674eef613d379c46c935111b9735
Carlivats <krlivat27@gmail.com>
2026-01-28 21:12:54 -0500

    Added Capture & Save Photos Test

---
+++ src/test_camera.py
@@ -0,0 +1,21 @@
+from picamera2 import Picamera2, Preview
+import os
+import time
+
+picam2 = Picamera2()
+
+config = picam2.create_preview_configuration({"size": (1280, 720)})
+picam2.configure(config)
+
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+
+time.sleep(5)
+
+if not os.path.exists("images"):
+    os.makedirs("images")
+
+picam2.capture_file("images/test.jpg")
+
+# Stop the camera
+picam2.stop()
